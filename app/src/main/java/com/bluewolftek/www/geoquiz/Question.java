60c05d33414b56fe97f18d39337d6c4788c16f03
commakozzi <commakozzi1@gmail.com>
2016-04-18 19:23:03 -0400

    Initial commit

---
+++ app/src/main/java/com/bluewolftek/www/geoquiz/Question.java
@@ -0,0 +1,35 @@
+package com.bluewolftek.www.geoquiz;
+
+/**
+ * Created by commakozzi on 4/16/2016.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+
+        mAnswerTrue = answerTrue;
+    }
+
+}
